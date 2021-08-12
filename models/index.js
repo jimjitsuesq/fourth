a6a9c3642e357c993e9250065571dc2258fc6059
Jim Peters <jimjitsuesq@gmail.com>
2021-08-12 15:28:32 -0700

    Removed sqlite references

--- models/index.js
+++ models/index.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-// const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.js');
 const db = {};
 
@@ -21,13 +20,6 @@ const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
         socketPath: '/cloudsql/portfolio-acm-server:us-central1:curriculum',
     }
 });
-
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
 
 fs
   .readdirSync(__dirname)
